73e8083c8ad81f896e7467284a5c193657f1f8c4
unknown <muleshubhangi7@gmail.com>
2022-12-16 15:54:37 +0530

    first commit

---
+++ src/myProg/SampleProg.java
@@ -0,0 +1,13 @@
+package myProg;
+
+public class SampleProg {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+System.out.println("Hi Welcome");
+
+System.out.println("i");
+	}
+
+}
