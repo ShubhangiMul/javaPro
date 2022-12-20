937ed10b9e416333e5cee10caa2f4450bdad3954
unknown <muleshubhangi7@gmail.com>
2022-12-20 10:36:22 +0530

    add 2nd time changes in branch1

--- src/myProg/SampleProg.java
+++ src/myProg/SampleProg.java
@@ -5,7 +5,7 @@ public class SampleProg {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-System.out.printSystem.out.println("Hi Welcome");
+System.out.println("Hi Welcome");
 System.out.println("Hi Welcome");
 System.out.println("Hi Welcome");
 
