465ef4f2650df5dd0fb7af39477e81ab556a574d
unknown <muleshubhangi7@gmail.com>
2022-12-20 10:30:00 +0530

    add changes in branch1

--- src/myProg/SampleProg.java
+++ src/myProg/SampleProg.java
@@ -5,11 +5,11 @@ public class SampleProg {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
+System.out.printSystem.out.println("Hi Welcome");
+System.out.println("Hi Welcome");
 System.out.println("Hi Welcome");
 
-
-
-
+System.out.println("Hi Welcome");
 System.out.println("i am Shubh");
 	}
 
